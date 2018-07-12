d486908a33a38fa991255ae5ccd90d7d7df7b320
oxc <oxc@oxc.com>
2018-07-13 07:36:14 +0800

    up, 添加数据库连接并增加获取games列表的action及其相应reducer

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 一个基于mongodb+express+react+redux+reactRouter实现的crud项目
 
-### 进入目录 npm i
+### 进入目录 npm i, 然后npm start
 
-### 进入backend目录 npm i
+### 进入backend目录 npm i，然后全局安装nodemon，然后npm start
+
+### 注意事项
+    - 数据库使用的是mongoDB，进入项目前请安装mongoDB
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,19 @@
 import express from 'express'
+import mongodb from 'mongodb'
 
 const app = express()
+const dbUrl = "mongodb://localhost"
 
-app.listen(8000, () => console.log('Server is running on localhost:8000'))
+mongodb.MongoClient.connect(dbUrl, (err, client) => {
+  if (err) throw err
+
+  const db = client.db('crud')
+
+  app.get('/api/games', (req, res) => {
+    db.collection('games').find({}).toArray((err, games) => {
+      res.json({ games })
+    })
+  })
+
+  app.listen(8080, () => console.log('Server is running on localhost:8080'))
+})
