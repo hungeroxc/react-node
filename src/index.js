e2caab6af340fd9b85347819b58ade7ee3c636f2
oxc <oxc@oxc.com>
2018-07-12 08:18:34 +0800

    up 配置好redux开发者工具和日志打印redux-logger

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import App from './components/App'
 
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducers from './reducers'
+import {composeWithDevTools} from 'redux-devtools-extension'
 
-const store = createStore(rootReducers)
+import logger from 'redux-logger'
+
+const store = createStore(
+    rootReducers,
+    composeWithDevTools(
+        applyMiddleware(logger)
+    )
+)
 
 ReactDOM.render(
     <Provider store={store}>
